c53eb52707469a20d32272a788dccf0cdf6219a4
Rodney H <rodney.hammad@gmail.com>
2020-05-05 08:42:32 +0300

    Auto deploy from gitHub to Azure

---
+++ client/src/containers/checkout-container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import HeaderView from '../views/header-view/header-view';
+import CheckoutView from '../views/checkout-view/checkout-view';
+
+import './base.css';
+
+class CheckoutContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <HeaderView pageName="cart" cartCount="0"/>
+                // @ts-ignore
+                <CheckoutView />
+            </div>
+        );
+    }
+}
+
+export default CheckoutContainer;
